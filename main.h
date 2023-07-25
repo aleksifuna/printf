52a4b0038b5b76abbd59b351de2c8067b6411d4c
Leonie-Ash <leoniemasakha@gmail.com>
2023-07-25 00:46:06 -0700

    printf

--- main.h
+++ main.h
@@ -4,7 +4,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_str(char *str);
 int print_int(int i);
-int print_abs(unsigned int i);
 int print_uns(unsigned int i);
 #include <stddef.h>
 #endif
--- print-int.c
+++ print-int.c
@@ -16,33 +16,7 @@ int print_int(int i)
 		i = -i;
 	}
 
-	return (print_abs((unsigned int )i) + 1);
-}
-/**
- * print_abs - prints absolute i
- * @i: interger to be printed
- *
- * Return: number of characters printed
- */
-int print_abs(unsigned int i)
-{
-	int c;
-
-	c = 0;
-
-	if (i / 10 == 0)
-	{
-		_putchar((i % 10) + '0');
-		c++;
-	}
-	else
-	{
-		print_int(i / 10);
-
-		_putchar((i % 10) + '0');
-		c++;
-	}
-	return (c);
+	return (print_uns((unsigned int )i) + 1);
 }
 /**
  * print_uns - print unsigned integer
