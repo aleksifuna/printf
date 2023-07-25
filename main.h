7ea91882bde54f085e753065907b7672ccb289ce
Alex Sifuna <aleksifuna@gmail.com>
2023-07-25 16:57:31 +0300

    Delete main.h

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_str(char *str);
-int print_int(int i);
-int print_uns(unsigned int i);
-#include <stddef.h>
-#endif
